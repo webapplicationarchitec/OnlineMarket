2c80046f8ecd1543e898f4d2966f73f360a0bb75
thaihainguyen <58269491+thaihainguyen@users.noreply.github.com>
2020-05-03 22:11:56 -0500

    Update RegisterController.java
    Add Get/Post mappings

--- src/main/java/miu/edu/cs545/controller/RegisterController.java
+++ src/main/java/miu/edu/cs545/controller/RegisterController.java
@@ -5,12 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
 
     @GetMapping("/reg")
     public String doRegistration(@ModelAttribute(value="account") Account account){
-        return "registration";
+        return "user/registration";
     }
+
+    @GetMapping("/profile")
+    public String updateProfile(@ModelAttribute(value="account") Account account){
+        return "user/profile";
+    }
+
+
+    @PostMapping("/saveAccount")
+    public String saveAccount(@ModelAttribute(value="account") Account account){
+        return "user/profile";
+    }
+
 }
