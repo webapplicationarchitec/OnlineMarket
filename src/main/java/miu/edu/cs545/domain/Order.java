5155c3386c284d161034aa204a003f8c2ebe9428
thaihainguyen <58269491+thaihainguyen@users.noreply.github.com>
2020-05-10 12:48:00 -0500

    Update Order.java

--- src/main/java/miu/edu/cs545/domain/Order.java
+++ src/main/java/miu/edu/cs545/domain/Order.java
@@ -5,14 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private Date dateDelivered;
     private Date dateShipping;
     private Integer tax;
@@ -22,4 +28,10 @@ public class Order {
     private Integer point;
     private String orderno;
     private OrderStatus status;
+
+    @OneToMany
+    @JoinColumn(name = "orderId")
+    private List<OrderDetail> listOrderDetail;
+
+
 }
