15d190bf08baec789043e110876e3694eab49b0d
thaihainguyen <58269491+thaihainguyen@users.noreply.github.com>
2020-05-10 12:48:06 -0500

    Update Product.java

--- src/main/java/miu/edu/cs545/domain/Product.java
+++ src/main/java/miu/edu/cs545/domain/Product.java
@@ -5,18 +5,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String name;
     private String description;
     private Double price;
     private Integer point;
     private String photo;
     private ProductStatus status;
-    private Integer categoryId;
+    @ManyToOne
+    @JoinColumn(name="categoryId")
     private Category category;
+
+    @OneToMany(mappedBy="product")
+    private List<Review> listReview;
 }
