805750b5819963f29f7d6e63cb55fe8c4e7d24af
thaihainguyen <58269491+thaihainguyen@users.noreply.github.com>
2020-05-10 12:48:03 -0500

    Update OrderDetail.java

--- src/main/java/miu/edu/cs545/domain/OrderDetail.java
+++ src/main/java/miu/edu/cs545/domain/OrderDetail.java
@@ -5,16 +5,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private Integer orderId;
-    private Order order;
-    private Integer productId;
+
+    @OneToOne
+    @JoinColumn(name="productId")
     private Product product;
-    private Double price;
+
+    private Double sellPrice;
     private Integer qty;
 }
