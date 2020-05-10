4acb40328c8f18e55be313e8c852d91af9cecc4c
thaihainguyen <58269491+thaihainguyen@users.noreply.github.com>
2020-05-10 12:47:56 -0500

    Update Category.java

--- src/main/java/miu/edu/cs545/domain/Category.java
+++ src/main/java/miu/edu/cs545/domain/Category.java
@@ -5,12 +5,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Category {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @NotNull
+    @Column(nullable = false)
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private List<Product> products;
 }
