e07f07371cbfd8f036e635273ec396644b464bda
khanhha0212 <khanhha0212@gmail.com>
2020-05-11 17:07:44 -0500

    done cart

--- src/main/java/miu/edu/cs545/dto/Cart.java
+++ src/main/java/miu/edu/cs545/dto/Cart.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import miu.edu.cs545.domain.OnlineOrder;
+import miu.edu.cs545.domain.Seller;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class Cart {
-    private List<OnlineOrder> orderList;
+    private Seller seller;
+    private HashMap<String, OnlineOrder> orderList;
 }
