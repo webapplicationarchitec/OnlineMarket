ac90a25dfad306c57befc1aca303b05a7944af17
ThanhLuan <luan.tt87@gmail.com>
2020-05-11 01:05:01 -0500

    add unit test ProductRepository

--- src/test/java/miu/edu/cs545/repositoryimpl/ProductRepositoryTest.java
+++ src/test/java/miu/edu/cs545/repositoryimpl/ProductRepositoryTest.java
@@ -1,10 +1,25 @@
 package miu.edu.cs545.repositoryimpl;
 
+import miu.edu.cs545.domain.Product;
+import miu.edu.cs545.repository.ProductRepository;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
+import java.util.Optional;
 
 @DataJpaTest
-public class ProductRepository {
+public class ProductRepositoryTest {
     @Autowired
     ProductRepository productRepository;
+
+    @Test
+    public void findById() throws Exception{
+        Optional<Product> productOptional = productRepository.findById(1);
+        assertEquals("Headphones", productOptional.get().getName());
+    }
 }
