d4ad970ebf5fccef89c64e4647f889a178b9c00b
ThanhLuan <luan.tt87@gmail.com>
2020-05-13 18:34:19 -0500

    add created cloumn in product

---
+++ src/main/java/miu/edu/cs545/exception/ExceptionsHandler.java
@@ -0,0 +1,26 @@
+package miu.edu.cs545.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleError(HttpServletRequest req, Exception exception) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("invalidProductId", exception.getProductId());
+		mav.addObject("exception", exception);
+		mav.addObject("url", req.getRequestURL() + "?" + req.getQueryString());
+		mav.setViewName("productNotFound");
+		return mav;
+	}
+
+}
