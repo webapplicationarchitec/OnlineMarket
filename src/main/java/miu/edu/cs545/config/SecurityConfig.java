4fd9b1bdb61788e296e6c74c9ca16bc3a365280b
ThanhLuan <luan.tt87@gmail.com>
2020-05-14 01:09:08 -0500

    remember me

--- src/main/java/miu/edu/cs545/config/SecurityConfig.java
+++ src/main/java/miu/edu/cs545/config/SecurityConfig.java
@@ -15,13 +15,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Autowired
     public SecurityConfig(@Qualifier("userDetailsServiceImp") UserDetailsService userDetailsService) {
@@ -44,17 +51,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+
         http.authorizeRequests()
                 .antMatchers("/**").permitAll()
-                //.antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**").permitAll()
-                //.anyRequest().authenticated()//all other urls can be access by any authenticated role
-                .and().formLogin().loginPage("/login").successHandler(new MyAuthenticationSuccessHandler())
+                .anyRequest().authenticated() //all other urls can be access by any authenticated role
+                .and()
+                .formLogin() //enable form login instead of basic login
+                .loginPage("/login").successHandler(new MyAuthenticationSuccessHandler())
+                .failureUrl("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
                 .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login").invalidateHttpSession(true).deleteCookies("JSESSIONID")
-                .and().csrf().ignoringAntMatchers("/h2-console/**")
-                .and().csrf().ignoringAntMatchers("/add-to-cart/**")
-                .and().headers().frameOptions().sameOrigin()
-                .and().exceptionHandling().accessDeniedPage("/access-denied");
+                .logoutSuccessUrl("/login").invalidateHttpSession(true)//.deleteCookies("JSESSIONID")
+                .and().csrf()
+                .ignoringAntMatchers("/h2-console/**") //don't apply CSRF protection to /h2-console
+                .and()
+                .exceptionHandling().accessDeniedPage("/error/access-denied")
+                .and().rememberMe().key("uniqueAndSecret")
+                .userDetailsService(userDetailsService).rememberMeParameter("remember-me").tokenRepository(tokenRepository()) //remmber me by token base
+        ;
+        // http.rememberMe().rememberMeParameter("remember-me").key("uniqueAndSecret"); //remmber me by cookie base
+        http.headers().frameOptions().disable();
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl=new JdbcTokenRepositoryImpl();
+        jdbcTokenRepositoryImpl.setDataSource(dataSource);
+        return jdbcTokenRepositoryImpl;
     }
 
 
